c78d126ca9cf1b826549af9195a7dacd55f4896f
RaIN <razor13372k@gmail.com>
2016-08-25 21:32:37 +0200

    Name Change

--- [Elobuddy] RaIN AIO BETA/Properties/AssemblyInfo.cs
+++ [Elobuddy] RaIN AIO BETA/Properties/AssemblyInfo.cs
@@ -1,13 +1,13 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
 
-[assembly: AssemblyTitle("RaIN AIO BETA")]
+[assembly: AssemblyTitle("T2IN1 AIO")]
 [assembly: AssemblyDescription("Elobuddy Script")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("League Gaming")]
-[assembly: AssemblyProduct("RaIN AIO BETA")]
-[assembly: AssemblyCopyright("Copyright © League Gaming 2016")]
-[assembly: AssemblyTrademark("League Gaming")]
+[assembly: AssemblyProduct("T2IN1 AIO")]
+[assembly: AssemblyCopyright("Copyright © T2IN1AIO 2016")]
+[assembly: AssemblyTrademark("T2IN1 AIO")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
 [assembly: Guid("ace38cee-7b1f-47ce-aa83-a71fe2949919")]
