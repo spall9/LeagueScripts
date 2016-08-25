3a7e3106c554805764a11c5889d845af76969cb1
RaIN <razor13372k@gmail.com>
2016-08-25 21:27:16 +0200

    Name Change

--- [Elobuddy] RaIN AIO BETA/Champions/Teemo/Modes/TeemoLastHit.cs
+++ [Elobuddy] RaIN AIO BETA/Champions/Teemo/Modes/TeemoLastHit.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using EloBuddy;
-using EloBuddy.SDK;
-using EloBuddy.SDK.Enumerations;
-using EloBuddy.SDK.Menu.Values;
-using Mario_s_Lib;
+﻿using Mario_s_Lib;
 using Mario_s_Template;
 using static T2IN1.TeemoMenu;
 using static T2IN1.TeemoSpells;
