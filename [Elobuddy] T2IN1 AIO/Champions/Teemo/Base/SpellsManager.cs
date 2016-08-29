db70af3e2e3b0a33dc3661980e2ef5069d0ae433
RaIN <razor13372k@gmail.com>
2016-08-29 13:33:09 +0200

    Change

--- [Elobuddy] T2IN1 AIO/Champions/Teemo/Base/SpellsManager.cs
+++ [Elobuddy] T2IN1 AIO/Champions/Teemo/Base/SpellsManager.cs
@@ -71,7 +71,7 @@ namespace T2IN1_Teemo
                 case SpellSlot.W:
                     if (W.IsReady())
                     {
-                        dmg += new float[] { 0, 0, 0, 0, 0 }[sLevel] + 0f*AP;
+                        dmg += new float[] { 0, 0, 0, 0, 0 }[sLevel];
                     }
                     break;
                 case SpellSlot.E:
