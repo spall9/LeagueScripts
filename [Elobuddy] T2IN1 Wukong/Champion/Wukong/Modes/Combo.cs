f240509ae5ea85a6147112f289f7b4e1c52b9179
RaIN <razor13372k@gmail.com>
2016-10-01 17:55:20 +0200

    Small Combo Change

--- [Elobuddy] T2IN1 Wukong/Champion/Wukong/Modes/Combo.cs
+++ [Elobuddy] T2IN1 Wukong/Champion/Wukong/Modes/Combo.cs
@@ -16,17 +16,17 @@ namespace T2IN1_Wukong
             var etarget = TargetSelector.GetTarget(E.Range, DamageType.Physical);
             var rtarget = TargetSelector.GetTarget(R.Range, DamageType.Physical);
 
+            if (ComboMenu["E"].Cast<CheckBox>().CurrentValue)
+                if (E.IsReady() && E.IsLearned && etarget.IsValidTarget(E.Range))
+                    E.TryToCast(etarget, ComboMenu);
+
             if (ComboMenu["Q"].Cast<CheckBox>().CurrentValue)
-                if (Q.IsReady() && E.IsOnCooldown && Q.IsLearned && qtarget.IsValidTarget(Q.Range))
+                if (Q.IsReady() && Q.IsLearned && qtarget.IsValidTarget(Q.Range))
                     Q.TryToCast(qtarget, ComboMenu);
 
             if (ComboMenu["W"].Cast<CheckBox>().CurrentValue)
                 if (W.IsReady() && W.IsLearned && wtarget.IsValidTarget(W.Range))
                     W.TryToCast(wtarget, ComboMenu);
-
-            if (ComboMenu["E"].Cast<CheckBox>().CurrentValue)
-                if (E.IsReady() && E.IsLearned && etarget.IsValidTarget(E.Range))
-                    E.TryToCast(etarget, ComboMenu);
 
             if (ComboMenu["R"].Cast<CheckBox>().CurrentValue)
                 if (E.IsOnCooldown && Q.IsOnCooldown && R.IsReady() && R.IsLearned && rtarget.IsValidTarget(R.Range))
