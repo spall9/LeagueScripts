9eeb425acb1062e2796b5a7352d52677b9aa72f4
RaIN <razor13372k@gmail.com>
2016-10-03 02:46:37 +0200

    Loader Color Change

--- [Elobuddy] T2IN1 Wukong/Loader.cs
+++ [Elobuddy] T2IN1 Wukong/Loader.cs
@@ -25,7 +25,7 @@ namespace T2IN1
                 JungleClear.Init();
 
                 if (ObjectManager.Player.ChampionName.Equals("MonkeyKing"))
-                    Chat.Print("[T2IN1]", TextColor.PaleVioletRed + "Wukong Loaded!", TextColor.SteelBlue);
+                    Chat.Print("[T2IN1]", TextColor.LimeGreen + "Wukong Loaded!", TextColor.OrangeRed);
                 else
                     Chat.Print("[T2IN1] " + ObjectManager.Player.ChampionName + " is not Supported!", TextColor.PaleVioletRed);
             }
