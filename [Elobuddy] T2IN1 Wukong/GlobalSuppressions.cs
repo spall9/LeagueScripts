d599f923a34deb46a41aebdd682cd4c17f09c86d
RaIN <razor13372k@gmail.com>
2016-10-05 03:55:20 +0200

    Small Update

---
+++ [Elobuddy] T2IN1 Wukong/GlobalSuppressions.cs
@@ -0,0 +1,18 @@
+﻿
+// This file is used by Code Analysis to maintain SuppressMessage 
+// attributes that are applied to this project.
+// Project-level suppressions either have no target or are given 
+// a specific target and scoped to a namespace, type, member, etc.
+
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:T2IN1_Wukong.Menus.CreateMenu")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~F:T2IN1_Wukong.Menus.DrawingsMenuID")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~F:T2IN1_Wukong.Menus.MiscMenuID")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~F:T2IN1_Wukong.Spells.Ignite")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:T2IN1.Loader.Loading_OnLoadingComplete(System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:T2IN1_Wukong.DamageIndicator.Drawing_OnEndScene(System.EventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:T2IN1_Wukong.DamageIndicator.Init")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~M:T2IN1_Wukong.ModeManager.AntiGapcloser_OnEnemyGapcloser(EloBuddy.AIHeroClient,EloBuddy.SDK.Events.Gapcloser.GapcloserEventArgs)")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "namespace", Target = "~N:T2IN1_Wukong")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "member", Target = "~P:T2IN1_Wukong.Spells.SmiteNames")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>", Scope = "type", Target = "~T:T2IN1_Wukong.SpellsManager")]
+[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Spellchecker", "CRRSP01:A misspelled word has been found.", Justification = "<Ausstehend>")]
